6997810029ed179942e9ccbc5c31ee442a83b92f
hackerrank <me@hackerrank.com>
2020-03-17 17:08:40 -0500

    Added Pet Person And PArk Model

---
+++ PupStuff/PupStuff/Models/DogPark.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PupStuff.Models
+{
+    public class DogPark
+    {
+        [Key]
+        public int ParkId { get; set; }
+        public string Address { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string Zipcode { get; set; }
+        public string Lat { get; set; }
+        public string Lng { get; set; }
+    }
+}
---
+++ PupStuff/PupStuff/Models/Pet.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PupStuff.Models
+{
+    public class Pet
+    {
+        [Key]
+        public int PetID { get; set; }
+        public string PetName { get; set; }
+        public int PetAge { get; set; }
+        public string PetPersonalityColor { get; set; }
+
+    }
+}
