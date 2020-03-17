7655d71e320aaa51338e61b243f72532d2f75a9a
hackerrank <me@hackerrank.com>
2020-03-17 17:10:53 -0500

    Added first few models

--- PupStuff/PupStuff/Models/DogPark.cs
+++ PupStuff/PupStuff/Models/DogPark.cs
@@ -10,6 +10,7 @@ namespace PupStuff.Models
     {
         [Key]
         public int ParkId { get; set; }
+        public string ParkName { get; set; }
         public string Address { get; set; }
         public string City { get; set; }
         public string State { get; set; }
